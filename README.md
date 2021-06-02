e47afe0cb92c0b3993b0f765f34c3bb930267f44
AyamBrando0o <illuminatin42@gmail.com>
2021-06-02 09:18:23 +0800

    added 3 functions in myscripts.js, almost complete little touch up left

---
+++ README.md
@@ -0,0 +1,1 @@
+Hi, this project is to create a simple scissors paper stone using java script.
---
+++ myscripts.js
@@ -0,0 +1,73 @@
+function Computerplay(){
+    num = Math.floor(Math.random()*3);
+
+    switch(num){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Scissors";
+        case 2: 
+            return "Paper";
+
+    }
+
+};
+
+function play(human,computer){
+    if (human.toUpperCase() == "ROCK"){
+        switch(computer){
+            case "Rock":
+                return 0;
+            case "Scissors":
+                return 1;
+            case "Paper": 
+                return -1;
+        }
+    }
+
+    if (human.toUpperCase() == "PAPER"){
+        switch(computer){
+            case "Rock":
+                return 1;
+            case "Scissors":
+                return -1;
+            case "Paper": 
+                return 0;
+        }
+    }
+
+    if (human.toUpperCase() == "SCISSORS"){
+        switch(computer){
+            case "Rock":
+                return -1;
+            case "Scissors":
+                return 0;
+            case "Paper": 
+                return 1;
+        }
+    }
+
+}
+
+function game(){
+    for (i=0; i<5; i++){
+        playerselection = prompt("Please enter your choice");
+        Computerselection = Computerplay();
+        result = play(playerselection,Computerselection);
+        switch(result){
+            case 0:
+                console.log("Draw!");
+                break;
+            case 1: 
+                console.log("You Win!");
+                break;
+            case 2:
+                console.log("You Lose!");
+                break;
+        }
+    }
+}
+
+game();
+
+
